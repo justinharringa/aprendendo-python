b33d74e7570ee99b9bc834d6b4f685ec1709354c
Justin Harringa <jharringa@salesforce.com>
2020-04-26 12:43:25 -0700

    Add semana1

---
+++ semana1.py
@@ -0,0 +1,90 @@
+######################################################
+# Desafio 1
+######################################################
+# 1. Escolhe 6 parques de diversão em Orlando
+# 2. Escholha uma frase sobre o parque que você vai mostrar pra eles. Por exemplo
+#    Exemplo: O usuarío colocou "disney" e seu programa vai mostrar: "que massa, vai falar com Mickey que tenho chulé!"
+#
+# 3. Pergunte o usuarió qual parque de diversão em Orlando eles vão querer visitar.
+# 4. Se o usuario escolheu um dos seus 6 parques, mostrar sua mensagen divertida sobre o parque
+#    Exemplo: O usuarío colocou "disney" e seu programa vai mostrar: "que massa, vai falar com Mickey que tenho chulé!"
+#
+# 5. Se o usuario escholeu um parque diferente, mostra um error divertido:
+#    Example: O usuarío colocou "Mirabilandia" e seu programa vai mostrar: "eita! eu não conheco Mirabilandia"
+#
+######################################################
+# Desafio 2
+######################################################
+# Tem alguns brinquedos no parque que não permitem pessoas abaixo de uma determinada altura usarlos
+#
+# Brinquedo                        Altura mínima em centimitros
+# Tomorrowland Speedway	           82
+# The Barnstormer	               89
+# Seven Dwarfs Mine Train	       97
+# Big Thunder Mountain Railroad	   102
+# Splash Mountain	               102
+# Space Mountain	               112
+#
+# 1. Perguntar ao usuario quantos centimetros que ele tem. Depois imprimir todos os brinquedos que ele
+# pode entrar
+#
+# Exemplo:
+# A criança tem quantos centimetros? 101
+# A criança pode entrar nesses brinquedos:
+# Tomorrowland Speedway
+# The Barnstormer
+# Seven Dwarfs Mine Train
+#
+# Outro Exemplo:
+# A criança tem quantos centimetros? 102
+# A criança pode entrar nesses brinquedos:
+# Tomorrowland Speedway
+# The Barnstormer
+# Seven Dwarfs Mine Train
+# Big Thunder Mountain Railroad
+# Splash Mountain
+#
+######################################################
+# Desafio 3
+######################################################
+# Vamos comprar comida no snack bar da Disney!
+# Um hot dog custa $7.00
+# Um hamburger custa $12.25
+# Um milkshake custa $8.50
+# Uma Coca Cola custa $4.00
+# Pergunta ao usuarío quantos hot dogs eles querem, depois pergunta quantos hamburgers,
+# depois pergunta quantos milkshakes, e finalmente pergunta quantas Coca Colas eles querem.
+# Aí vocé vai somar o custo e mostra quanto eles vão pagar
+#
+# Exemplo:
+# Quantos hot dogs você quer? 1
+# Quantos hamburgers você quer? 1
+# Quantos milkshakes você quer? 2
+# Quantas Coca Colas você quer? 0
+# Certo, você vai ter que pagar $36.25
+#
+######################################################
+# Desafio 4
+######################################################
+# Vamos ver se você tem dinheiro suficiente pra comprar comida em qualquer snack bar do Disney!
+# Aí o usuario vai colocar quanto dinheiro eles tem, quanta custa cada opção, e quantas eles vão querer.
+# Seu programa vai dizer se eles tem dinheiro suficiente
+# Se tem dinheiro suficiente pra comprar tudo, o programa vai dizer quanto você recebe de troco
+# Se não tem dinheiro suficiente, o programa diz quanto dinheiro falta pra comprar tudinho
+#
+# Dica: lembra que você vai ter que usar o método float(). Se você usa int(), o computador vai arredondar o numero
+#
+# Esse exercicio é parecido com desafio 3. Você pode escholher quais são as opções.
+#
+# Esse exemplo vai usar as mesmas opções do outro exercicio:
+# Exemplo:
+# Quanto dinheiro você tem? 52
+# Bem, quanto custa um hot dog? 7.00
+# Quanto custa um hamburger? 12.25
+# Quanto custa um milkshake? 8.50
+# Quanto custa uma Coca Cola? 4.00
+# Exelente! Aí quantos hot dogs você quer? 1
+# Quantos hamburgers você quer? 1
+# Quantos milkshakes você quer? 2
+# Quantas Coca Colas você quer? 0
+# OPA! Você tem dinheiro suficiente! Aproveite! Aí você ainda vai ter $15.75
